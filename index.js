1021ad0799e61354bd01aa86c90ce2cdb6d8da96
Mainak Basu <kaniam@Mainaks-MacBook-Pro.local>
2021-06-05 22:14:29 -0400

    first push

---
+++ index.js
@@ -0,0 +1,15 @@
+//Load express module with `require` directive
+var express = require("express");
+var app = express();
+
+//Define request response in root URL (/)
+app.get("/", function (req, res) {
+  res.json({
+    message: `${process.pid}: Hello world!!!!`,
+  });
+});
+
+//Launch listening server on port 3000
+app.listen(5000, function () {
+  console.log("app listening on port http://localhost:5000!");
+});
